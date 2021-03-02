aa8bf9c004519ee246b854d13a686274929c5928
Piotr Chmielowski <piotr.a.chmielowski@gmail.com>
2021-03-02 13:04:57 +0100

    Rename

--- ui/src/main/java/net/chmielowski/baggage/ui/EquipmentListFragment.kt
+++ ui/src/main/java/net/chmielowski/baggage/ui/EquipmentListFragment.kt
@@ -3,12 +3,12 @@ package net.chmielowski.baggage.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import net.chmielowski.baggage.ui.databinding.ScreenItemsBinding
+import net.chmielowski.baggage.ui.databinding.ScreenEquipmentListBinding
 
-class MainFragment : Fragment(R.layout.screen_items) {
+class EquipmentListFragment : Fragment(R.layout.screen_equipment_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val binding = ScreenItemsBinding.bind(view)
+        val binding = ScreenEquipmentListBinding.bind(view)
         val adapter = EquipmentAdapter()
         binding.list.adapter = adapter
 
