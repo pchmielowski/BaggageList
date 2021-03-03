725269819bf7951db5be35a04fd25a9b7e52976e
Piotr Chmielowski <piotr.a.chmielowski@gmail.com>
2021-03-03 21:39:13 +0100

    View model

--- ui/src/main/java/net/chmielowski/baggage/ui/EquipmentListFragment.kt
+++ ui/src/main/java/net/chmielowski/baggage/ui/EquipmentListFragment.kt
@@ -3,11 +3,19 @@ package net.chmielowski.baggage.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.arkivanov.mvikotlin.core.store.Reducer
+import com.arkivanov.mvikotlin.extensions.coroutines.SuspendExecutor
+import com.arkivanov.mvikotlin.extensions.coroutines.labels
+import com.arkivanov.mvikotlin.extensions.coroutines.states
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
+import kotlinx.coroutines.flow.Flow
 import net.chmielowski.baggage.ui.databinding.ScreenEquipmentListBinding
 
 class EquipmentListFragment : Fragment(R.layout.screen_equipment_list) {
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val model by viewModels<EquipmentListViewModel>()
         val binding = ScreenEquipmentListBinding.bind(view)
         val adapter = EquipmentAdapter()
         binding.list.adapter = adapter
@@ -15,3 +23,45 @@ class EquipmentListFragment : Fragment(R.layout.screen_equipment_list) {
         adapter.submitList((1..30).map { EquipmentItem(EquipmentId(it), "Item $it") })
     }
 }
+
+class EquipmentListViewModel : ViewModel() {
+
+    private val storeFactory = DefaultStoreFactory
+
+    private val store = storeFactory.create(
+        name = "EquipmentListStore",
+        initialState = State(),
+        executorFactory = EquipmentListViewModel::Executor,
+        reducer = ReducerImpl(),
+    )
+
+    fun observeModel(): Flow<Model> = store.states
+
+    fun observeLabels() = store.labels
+
+    sealed class Intent
+
+    private sealed class Result
+
+    sealed class Label
+
+    private data class State(
+        val equipmentList: List<EquipmentDto> = emptyList(),
+    ) : Model
+
+    interface Model
+
+    private class Executor : SuspendExecutor<Intent, Nothing, State, Result, Label>()
+
+    private class ReducerImpl : Reducer<State, Result> {
+
+        override fun State.reduce(result: Result): State {
+            return this
+        }
+    }
+}
+
+data class EquipmentDto(
+    val id: EquipmentId,
+    val name: String,
+)
