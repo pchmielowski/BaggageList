78d637210d875bf3a8e7b394ddb63853c4aa45b4
Piotr Chmielowski <piotr.a.chmielowski@gmail.com>
2021-03-03 21:45:55 +0100

    Koin

---
+++ ui/src/main/java/net/chmielowski/baggage/ui/BaggageApplication.kt
@@ -0,0 +1,19 @@
+package net.chmielowski.baggage.ui
+
+import android.app.Application
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+@Suppress("unused")
+class BaggageApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            module {
+                viewModel { EquipmentListViewModel() }
+            }
+        }
+    }
+}
