27ce339c3fa983112def4a39bd05d76a53af8108
Piotr Chmielowski <piotr.a.chmielowski@gmail.com>
2021-03-08 13:47:53 +0100

    Disable view binding for MainActivity

--- ui/src/main/java/net/chmielowski/baggage/ui/MainActivity.kt
+++ ui/src/main/java/net/chmielowski/baggage/ui/MainActivity.kt
@@ -1,14 +1,5 @@
 package net.chmielowski.baggage.ui
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import net.chmielowski.baggage.ui.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
